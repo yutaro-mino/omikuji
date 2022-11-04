11651116e21e6ec095b9bfd273a9447b8e65a8a6
DESKTOP-UBVG5Q0\yutaro <yutaro12140408@gmail.com>
2022-11-04 18:42:50 +0900

    Initial Commit

--- omikuji.js
+++ omikuji.js
@@ -4,6 +4,10 @@ let userresult;
 username=prompt("お名前を教えて下さい")
 document.getElementById("name").innerHTML = username;
 
+if(username==""){
+  document.getElementById("name").innerHTML="名無し"
+}
+
 let rand = Math.floor( Math.random() * 5) ;
 if(rand==0){
     userresult="大吉"
